34e1a0990e8281e7aee823cab1ad837488f979e0
flamableassassin <flamableassassin@users.noreply.github.com>
2020-02-13 23:22:20 +0000

    :art: Changing how error are handled

--- src/structures/tag.js
+++ src/structures/tag.js
@@ -2,20 +2,27 @@ class Tag {
   constructor(tag = {}) {
     Object.assign(this, tag)
   }
-
+/*
+The codes:
+  0 = It worked
+  1 = Missing input
+  2 = No database argument
+  3 = No tag found
+  4 = Tag already exists
+*/
   date(time = new Date()) {
     this.created=time
     return this;
   }
 
   owner(owner){
-    if(owner===undefined)throw "No user inputted";
+    if(owner===undefined)return "1";
     this.owner=owner
     return this;
   }
 
   name(name){
-    if(name===undefined)throw "No name inputted";
+    if(name===undefined)return "1";
     this.name=name
     return this;
   }
@@ -26,36 +33,36 @@ class Tag {
   }
 
   create(db){
-    if(db===undefined)throw "No database passed";
+    if(db===undefined)return "2";
     //checking if it already a tag//
     db.db("data").collection("tags").findOne({
       name:this.name
     },function (err,result) {
       if(err)throw err;
-      if(result!==undefined)throw "That tag already exists";
+      if(result!==undefined)return "4";
       //creating tag//
       db.db("data").collection("tags").insertOne(this,function (err,res) {
         if(err)throw err;
-        return "Tag created"
+        return "0"
       })
     })
   }
 
   edit(db){
-    if(db===undefined)throw "No database passed";
+    if(db===undefined)return "2";
     //checking for tag//
     db.db("data").collection("tags").findOne({
       name:this.name
     },function(err,res) {
       if(err)throw err;
-      if(res===undefined)throw "No tag found";
+      if(res===undefined)return "3";
       db.db("data").collection("tags").updateOne({
         name:this.name
       },{
         $set:this
       },function (err,res) {
         if(err)throw err
-        return "Tag updated";
+        return "0";
       })
     })
   }
