7625790c620b6ec24492a69994bc859ccfe4dc84
flamableassassin <flamableassassin@users.noreply.github.com>
2020-02-13 23:24:40 +0000

    :art:Updating

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -1,3 +1,4 @@
+const{Embed}=require("../../structures/index.js")
 module.exports = {
 	name: 'help',
 	description: 'the help command',
@@ -11,8 +12,16 @@ module.exports = {
   developer: false,//bot owner only
 	nsfw: false,//if the command needs to be used in a nsfw channel
 	execute(msg, args, client, db, config) {
-    if(args[0]===null){
-      
+    if(args.length===0){
+			const embed = new Embed()
+			 .color()
+			 .timestamp()
+			 .title("Here are my commands")
+			 .description(`Use \`${config.prefix}Help [Category]\`\nE.g. \`${config.prefix}Help Tag\`\n\`<>\` are needed for the command\n\`[]\` are need for the command`)
+			 .addField("__General Commands__","`Help` Shows you this beautiful message\n")
+			 .addField("__Tag Commands__","`Create <name> <message>` Creates a new tag. Names can only be one word\n`Delete <Name>` Deletes a tag\n`Edit <New Message>` Edits the message on a tag\n`Info <Name>` Info about the tag\nTag <Name>` Shows the tag")
+			 .addField("__Honks__","`View [@user]` Shows the number of honks the user has")
+			client.createMessage(msg.channel.id,embed)
     }
 	},
 };
