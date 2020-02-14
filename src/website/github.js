542fb85b6e248e3d3f9700de709480d1f28eeb48
flamableassassin <flamableassassin@users.noreply.github.com>
2020-02-14 21:19:07 +0000

    :bug: fixing bug

--- src/website/github.js
+++ src/website/github.js
@@ -1,13 +1,17 @@
-const {Octokit}=require("@octokit/rest")
-module.exports=function (handler,http,config) {
+const {
+  Octokit
+} = require("@octokit/rest")
+module.exports = function(handler, http, config) {
   handler.on('error', (error) => {
     console.error(error);
   })
 
-  handler.on('issues',(event) => {
-    if(event.payload.action==="opened"){
+  handler.on('issues', (event) => {
+    if (event.payload.action === "opened") {
       //addd msg to issue
-    }else if(event.payload.action ==="labeled")
+    } else if (event.payload.action === "labeled"){
+
+    }
 
   })
 }
