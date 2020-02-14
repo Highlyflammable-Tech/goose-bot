697e7dcc2c6eaa491d710d3984342ccf5fdc06a3
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2020-02-14 19:47:14 +0000

    :construction: started to build

--- src/website/index.js
+++ src/website/index.js
@@ -1,5 +1,26 @@
-const express = require('express');
-module.expots=function(client,db,config) {
-  //Soon!
-  return ;
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+module.expots = function(client, db, config) {
+  var handler = createHandler({
+    path: '/webhook',
+    secret: config.webhook_token
+  })
+  http.createServer(function(req, res) {
+      handler(req, res, function(err) {
+        res.statusCode = 404
+        res.end('no such location')
+      })
+    })
+    .listen(3000 || process.env.PORT)
+
+  handler.on('error', (error) => {
+    console.error(error);
+  })
+
+  handler.on('issues',(event) => {
+    if(event.payload.action==="opened"){
+      //addd msg to issue
+    }else if(event.payload.action ==="labeled")
+
+  })
 }
