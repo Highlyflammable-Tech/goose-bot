d2d59bd4218ea5000e4715c4f6eaf7954959947c
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2020-02-24 13:14:17 +0000

    :pencil: Making it easier to create embeds

--- src/structures/embed.js
+++ src/structures/embed.js
@@ -1,25 +1,38 @@
+/**
+ * Used to build embeds
+ * @param {Object} embed=Object An existing embed that can be edited.
+ */
 class Embed {
   constructor(embed = {
     "embed": {
       "fields": []
     },
     "content": ""
-  }){
+  }) {
     Object.assign(this, embed)
     return this;
   }
-
+  /**
+   * @param {string} message The message to be added.
+   */
   message(content = "") {
     this.content = content
     return this;
   }
-
+  /**
+   * @param {string} title The title of the embed
+   * @param {string} url="" The URL in the title
+   */
   title(title, url = "") {
     this.embed.title = title.toString().substring(0, 256)
     this.embed.url = url
     return this;
   }
-
+  /**
+   * @param {string} name The message to be added.
+   * @param {string} icon="" The icon for the author
+   * @param {string} url="" The url in the author
+   */
   author(name, icon = "", url = "") {
     this.embed.author = {
       "name": name.toString().substring(0, 256),
@@ -28,17 +41,25 @@ class Embed {
     }
     return this;
   }
-
+  /**
+   * @param {string} color=13873944 The colour of the embed.
+   * Can use hex.
+   */
   color(color = 13873944) {
     this.embed.color = (color[0] === "#") ? parseInt(color.replace("#", ""), 16) : color
     return this;
   }
-
+  /**
+   * @param {date} time=Date() The timestamp on the embed
+   */
   timestamp(time = new Date()) {
     this.embed.timestamp = time
     return this;
   }
 
+  /**
+   * @param {string} image The thumnail on the embed
+   */
   thumnail(image) {
     this.embed.thumnail = {
       "url": image
@@ -46,26 +67,39 @@ class Embed {
     return this;
   }
 
+  /**
+   * @param {string} image The image on the embed
+   */
   image(image) {
     this.embed.image = {
       "url": image
     }
     return this;
   }
-
-  footer(text, url = "") {
+  /**
+   * @param {string} text The text in the footer
+   * @param {string} icon The icon in the footer
+   */
+  footer(text, icon = "") {
     this.embed.footer = {
-      "icon_url": url,
+      "icon_url": icon,
       "text": text
     }
     return this;
   }
 
+  /**
+   * @param {string} value The text in the description of the embed
+   */
   description(value) {
     this.embed.description = value.toString().substring(0, 2048)
     return this;
   }
-
+  /**
+   * @param {string} title The title of the field
+   * @param {string} value The text inside the field
+   * @param {boolean} inline If the field should be inline
+   */
   addField(title, value, inline = false) {
     this.embed.fields.push({
       "name": title.toString().substring(0, 256),
@@ -75,6 +109,11 @@ class Embed {
     return this;
   }
 
+  /**
+   * Used to add a random image to the embed
+   * @param {Client} client The client that is running
+   * @param {Message} msg The msg that was received
+   */
   send(client, msg) {
     //random image//
     let images = ["", "", "https://file.coffee/pk1hzydPo.gif", "", "", "", "https://file.coffee/a4Ww80qFK.png", "https://file.coffee/kkLum4nVe.png", "https://file.coffee/0QfbTcfdr.png", "https://file.coffee/rXFMQIU0S.png", "https://file.coffee/CU-GVlaxI.png", "https://file.coffee/V41rtFtCm.png", "https://file.coffee/ipAyCS-6f.png"]
